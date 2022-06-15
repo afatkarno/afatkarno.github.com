7a6fecaec790810f610ba621894fe13eac34d0ed
afatkarno <36460502+afatkarno@users.noreply.github.com>
2022-06-15 10:53:59 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,40 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Hai Sayang Ku <3",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Hehe Iya Kamu",  // 同上...
+        "Happy Birthdayy Biyy",
+        "Semoga Sehat Selalu",
+        "Dilancarkan Rezekinya",
+        "Diberi Kemudahan Dalam Masalah Apapun",
+        "Tambah Cumill Jugaaaa",
+        "Dan Tambah Sayang Ke Aku Skskskkssk",
+        "LOVE YOUU~~",
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "Hehe Iya Kamu": "./imgs/xiaokeai.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "Hi, Touch Me biyy",
+        play: "Hi, Touch Me biyy",
+        bannar_coming: "Hi, Touch Me biyy",
+        balloons_flying: "Hi, Touch Me biyy",
+        cake_fadein: "Hi, Touch Me biyy",
+        light_candle: "Hi, Touch Me biyy",
+        wish_message: "Hi, Touch Me biyy",
+        story: "A MESSAGE FOR YOU",
+    }
+};
